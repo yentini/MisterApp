d073f61c51bbcd6ec1026c3af5c528da4af4bc18
César Cabrerizo de Gregorio <ccabrerizo@indra.es>
2023-02-08 08:17:08 +0100

    Muestra bien los jugadores en el dialogo de selección

--- app/src/main/java/com/example/misterapp/ui/team/components/AddTeamPlayersAlertDialog.kt
+++ app/src/main/java/com/example/misterapp/ui/team/components/AddTeamPlayersAlertDialog.kt
@@ -1,37 +1,21 @@
 package com.example.misterapp.ui.team.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLifecycleOwner
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.repeatOnLifecycle
-import com.example.misterapp.core.Alphabet
-import com.example.misterapp.core.Categorys
 import com.example.misterapp.core.Constants.Companion.ADD
-import com.example.misterapp.core.Constants.Companion.ADD_TEAM
-import com.example.misterapp.core.Constants.Companion.CATEGORY
-import com.example.misterapp.core.Constants.Companion.CLUB
-import com.example.misterapp.core.Constants.Companion.LEVEL
-import com.example.misterapp.core.Constants.Companion.NO_VALUE
-import com.example.misterapp.core.Constants.Companion.YEAR
 import com.example.misterapp.domain.model.PlayerModel
 import com.example.misterapp.ui.generic_components.ContentLoadingProgressBar
 import com.example.misterapp.ui.players.PlayersUiState
-import com.example.misterapp.ui.players.components.getBirthday
 import com.example.misterapp.ui.team.TeamViewModel
 import java.util.*
 
@@ -95,7 +79,7 @@ fun AddTeamPlayersAlertDialog(
                                 onDismiss()
                                 onTeamPlayersAdded(
                                     teamId,
-                                    listOf<PlayerModel>((uiPlayersState as PlayersUiState.Success).players.random())
+                                    (uiPlayersState as PlayersUiState.Success).players.filter { it.isSelected.value }
                                 )
                             }, modifier = Modifier
                                 .padding(horizontal = 8.dp)
